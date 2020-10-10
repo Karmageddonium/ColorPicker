dca34490bd72abd02f639640a4ae07fd9467a0d6
Александр <a.pchelkin@bk.ru>
2020-10-10 15:53:11 +0300

    Added .Net Core projects and moved projects to special folders Framework and Core

--- Framework/ColorPickerWPF/ColorPickerControl.xaml.cs
+++ Framework/ColorPickerWPF/ColorPickerControl.xaml.cs
@@ -7,7 +7,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using ColorPickerWPF.Code;
-using ColorPickerWPF.Properties;
 using UserControl = System.Windows.Controls.UserControl;
 
 namespace ColorPickerWPF
