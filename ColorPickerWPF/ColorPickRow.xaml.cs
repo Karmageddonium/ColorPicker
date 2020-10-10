7e7c361f5d780fc546f0c3754f7737759f0bf8eb
Александр <a.pchelkin@bk.ru>
2020-10-10 13:51:50 +0300

    Добавьте файлы проекта.

---
+++ ColorPickerWPF/ColorPickRow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using ColorPickerWPF.Code;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace ColorPickerWPF
+{
+    /// <summary>
+    /// Interaction logic for ColorPickRow.xaml
+    /// </summary>
+    public partial class ColorPickRow : UserControl
+    {
+        public event EventHandler OnPick;
+
+        public Color Color { get; set; }
+        
+        public ColorPickerDialogOptions Options { get; set; }
+
+        public ColorPickRow()
+        {
+            InitializeComponent();
+        }
+
+        private void PickColorButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Color color;
+            Color? initialColor = null;
+            if(ColorDisplayGrid.Background is SolidColorBrush scb)
+            {
+                initialColor = scb.Color;
+            }
+            if (ColorPickerWindow.ShowDialog(out color, Options, initialColor:initialColor))
+            {
+                SetColor(color);
+                OnPick?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void SetColor(Color color)
+        {
+            Color = color;
+            HexLabel.Text = color.ToHexString();
+            ColorDisplayGrid.Background = new SolidColorBrush(color);
+        }
+    }
+}
